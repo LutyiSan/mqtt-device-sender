d77e67b064a61a1cad8cf67180b2143665a3631e
LutyiSan <76662954+LutyiSan@users.noreply.github.com>
2022-08-18 11:55:01 +0300

    Update main.py

--- device-simulator/main.py
+++ device-simulator/main.py
@@ -25,7 +25,7 @@ class DeviceSimulator:
         self.client = mqttc.Client(client_id=self.publisher + str(randint(1234, 56789)), clean_session=True,
                                    userdata=None, protocol=mqttc.MQTTv311,
                                    transport=self.type)
-        self.devices = DEVICES  # devices which is emulated here
+        
 
     def create_client(self):
         self.client.ws_set_options(path=self.url_path, headers=None)
@@ -36,30 +36,73 @@ class DeviceSimulator:
         # client.tls_insecure_set(True)
 
     def lift(self):
-        device = {"name": "lift", "stage-1": False, "stage-2": False, "stage-3": False, "stage-4": False,
-                "stage-5": False, "stage-6": False, "stage-7": False, "door": False}
-        stage = random.choice([1,2,3,4,5,6,7])
-        door = random.choice([True, False, False, True, True, False])
+        device = {"name": "lift", "stage11": False, "stage12": False, "stage13": False, "stage14": False,
+                "stage15": False, "stage16": False, "stage17": False,"stage21": False, "stage22": False, "stage23": False, "stage24": False,
+                "stage25": False, "stage26": False, "stage27": False, "door": False, "door2" :False, "door3": False, "stage31": False, "stage32": False, "stage33": False, "stage34": False,
+                "stage35": False, "stage36": False, "stage37": False,}
+        stage1 = random.choice([11,12,13,14,15,16,17])
+        door1 = random.choice([True, False, False, True, True, False])
+        stage2 = random.choice([21,22,23,24,25,26,27])
+        door2 = random.choice([True, False, False, True, True, False])
+        stage3 = random.choice([31,32,33,34,35,36,37])
+        door3 = random.choice([True, False, False, True, True, False])
         for i in device.keys():
             #print(i)
-            if str(stage) in i:
-                print(str(stage))
-                print (i)
+            if str(stage1) in i:
+              #  print(str(stage))
+              #  print (i)
                 device[i] = True
-            device["door"] = door
+                device["door"] = door1
+  
+            if str(stage2) in i:
+              #  print(str(stage))
+              #  print (i)
+                device[i] = True
+                device["door2"] = door2
+        
+     
+            if str(stage3) in i:
+              #  print(str(stage))
+              #  print (i)
+                device[i] = True
+                device["door3"] = door3           
         self.send_json = json.dumps(device)
         self.client.publish(f"lucenko/{device['name']}", payload=self.send_json, qos=0, retain=True)
         logger.debug(f"Publish topic lucenko/{device['name']}...")
+        
+    def scud(self):
+        device = {"name": "scud", "td1": False, "td1alarm": False, "td2": False, "td2alarm": False,
+                "td3": False, "alarm": False, "td4": False, "td4alarm": False}
+        td = random.choice([1,2,3,4])
+        #door = random.choice([True, False, False, True, True, False])
+        for i in device.keys():
+            #print(i)
+            if str(td) in i:
+                #print(str(td))
+                #print (i)
+                device[f'td{td}'] = True
+                alarm = random.choice([False, False, False, False,True,False,False, False, False,True])
+                device[f'td{td}alarm'] = alarm
+                break
+                
+                
 
+
+        self.send_json = json.dumps(device)
+        self.client.publish(f"lucenko/{device['name']}", payload=self.send_json, qos=0, retain=True)
+        logger.debug(f"Publish topic lucenko/{device['name']}...")
 
     def connect_client(self):
         self.client.connect(self.broker, port=self.broker_port, keepalive=60, bind_address="")
         logger.debug(f"Connection to {self.broker}:{self.broker_port} READY")
 
     def publishing(self):
-        for device in self.devices:
+        devices = DEVICES
+        for device in devices:
             if device['name'] == "lift":
                 self.lift()
+            elif device['name'] == "scud":
+                self.scud()
             else:
                 for key, value in device.items():
                     if key == 'name':
